866e25110c916f9c2d3fb22ca4a3bdf72ece0565
HiroUrata <1712026@i.softbank.jp>
2021-08-21 18:02:20 +0900

    Initial Commit

---
+++ scrollHiddenView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scrollHiddenView
+//
+//  Created by UrataHiroki on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
