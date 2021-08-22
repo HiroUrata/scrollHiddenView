0fb954497e241274631a3fc08828d2fbaa56f6a3
HiroUrata <1712026@i.softbank.jp>
2021-08-22 17:22:23 +0900

    tableViewをスクロール時に、moveViewを上下動をさせる　

--- scrollHiddenView/ViewController.swift
+++ scrollHiddenView/ViewController.swift
@@ -30,11 +30,11 @@ class ViewController: UIViewController {
         moveView.frame = CGRect(x: view.frame.minX + 10, y: view.frame.maxY - (view.frame.maxY / 7), width: view.frame.size.width - 20, height: view.frame.size.height / 15)
         
         moveView.layer.cornerRadius = 30.0
-        moveView.backgroundColor = .tertiarySystemGroupedBackground
+        moveView.backgroundColor = .systemGreen
         
-        moveView.layer.shadowOffset = CGSize(width: 10, height: 10)
-        moveView.layer.shadowOpacity = 0.5
-        moveView.layer.shadowRadius = 7
+        moveView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        moveView.layer.shadowOpacity = 1
+        moveView.layer.shadowRadius = 0
         
         view.addSubview(moveView)
         
@@ -66,14 +66,60 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource {
 
         if moveView.frame.origin.y == view.frame.maxY - (view.frame.maxY / 7){
 
+            UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY - self.moveView.frame.height}, completion: nil)
+
+        }
+
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+
+        var judgeCGFloat = tableView.contentOffset.y
+
+        if judgeCGFloat > 0{
+
             UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY}, completion: nil)
+
+            judgeCGFloat = 0
+
+        }else if judgeCGFloat < 0{
+
+            UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY - (self.view.frame.maxY / 7)}, completion: nil)
+
+            judgeCGFloat = 0
+
+        }
+
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        if moveView.frame.origin.y == self.view.frame.maxY - self.moveView.frame.height{
+
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY}, completion: nil)
+
+        }
+    }
+    
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+
+        if moveView.frame.origin.y == self.view.frame.maxY{
+
+            UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY - (self.view.frame.maxY / 7)}, completion: nil)
 
         }
 
     }
    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {self.moveView.frame.origin.y =  self.view.frame.maxY - (self.view.frame.maxY / 7)}, completion: nil)
+
+        
+    }
     
-//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    //    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
 //        print(tableView.contentOffset)
 //        if moveView.frame.origin.x == self.view.frame.minX - self.view.frame.size.width{
 //
